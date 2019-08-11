1872d56f6a13e55e7e53b7b4c08ddcdd5eb0732a
Mateusz Gil <mateusz_gil@outlook.com>
2019-08-11 12:45:29 +0200

    Improved adding front and back

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import implementations.CycledList;
 import implementations.QuickUnion;
 
 import java.util.Scanner;
@@ -6,24 +7,33 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
-        int p;
-        int q;
-        System.out.print("Type number of nodes: ");
-        int n = input.nextInt();
-        QuickUnion unionFind = new QuickUnion(n);
 
-        System.out.println("Type " + n + " connections between nodes (p q)");
-        for(int i = 0; i < n; i++)
-        {
-            p = input.nextInt();
-            q = input.nextInt();
-            unionFind.union(p, q);
-        }
+        CycledList<Integer> list = new CycledList<>();
+        System.out.println(list.isEmpty());
+        list.addBack(2);
+        System.out.println(list.getFront());
+      //  System.out.println(list.getBack());
+        list.addFront(3);
+        System.out.println(list.getFront());
 
-        System.out.println("Check connection: ");
-        p = input.nextInt();
-        q = input.nextInt();
-        System.out.println(unionFind.connected(p, q));
+//        Scanner input = new Scanner(System.in);
+//        int p;
+//        int q;
+//        System.out.print("Type number of nodes: ");
+//        int n = input.nextInt();
+//        QuickUnion unionFind = new QuickUnion(n);
+//
+//        System.out.println("Type " + n + " connections between nodes (p q)");
+//        for(int i = 0; i < n; i++)
+//        {
+//            p = input.nextInt();
+//            q = input.nextInt();
+//            unionFind.union(p, q);
+//        }
+//
+//        System.out.println("Check connection: ");
+//        p = input.nextInt();
+//        q = input.nextInt();
+//        System.out.println(unionFind.connected(p, q));
     }
 }
--- src/main/java/implementations/CycledList.java
+++ src/main/java/implementations/CycledList.java
@@ -8,6 +8,8 @@ public class CycledList<T>
         Node next;
         Node prev;
 
+        public Node() {}
+
         public Node(T value)
         {
             this.data = value;
@@ -27,10 +29,80 @@ public class CycledList<T>
     {
         Node newFront = new Node(value);
 
-        newFront.prev = this.head.prev;
-        newFront.next = this.head;
-        this.head.prev = newFront;
-        this.head = newFront;
+        if(this.isEmpty())
+        {
+            this.head = new Node(value);
+            this.head.next = this.head;
+            this.head.prev = this.head;
+        }
+
+        else
+        {
+            newFront.prev = this.head.prev;
+            newFront.next = this.head;
+            this.head.prev = newFront;
+            this.head = newFront;
+        }
+    }
+
+    public void addBack(T value)
+    {
+        if(this.isEmpty())
+        {
+            this.head = new Node(value);
+            this.head.next = this.head;
+            this.head.prev = this.head;
+        }
+        else
+        {
+            Node newBack = new Node(value);
+
+            newBack.prev = this.head.prev;
+            newBack.next = this.head;
+            this.head.prev = newBack;
+        }
+    }
+
+    public T popFront()
+    {
+        if(this.head != null)
+        {
+            T value = (T)this.head.next.data;
+            this.head.prev.next = this.head.next;
+            this.head.next.prev = this.head.prev;
+            this.head = this.head.next;
+
+            return value;
+        }
+
+        else  throw new NullPointerException("There is nothing to popFront");
+    }
+
+    public T popBack()
+    {
+        if(this.head != null)
+        {
+            T value = (T) this.head.prev.data;
+            this.head.prev.prev.next = this.head.prev;
+            this.head.prev = this.head.prev.prev;
+
+            return value;
+        }
+        else throw new NullPointerException("There is nothing to popBack");
+    }
+
+    public T getFront()
+    {
+        if(this.head != null) return (T) this.head.data;
+
+        else throw new NullPointerException("There is nothing in front");
+    }
+
+    public T getBack()
+    {
+        if(this.head.prev != null) return (T) this.head.prev.data;
+
+        else throw new NullPointerException("There is nothing in back");
     }
 
 }
