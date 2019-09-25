3d0586abe48acaa278aa04dbb806f0f249df9a20
Mateusz Gil <mateusz_gil@outlook.com>
2019-09-25 14:55:16 +0200

    added generate primes method

--- src/main/java/implementations/SieveOfErastothenes.java
+++ src/main/java/implementations/SieveOfErastothenes.java
@@ -1,15 +1,37 @@
 package implementations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SieveOfErastothenes
 {
     private int size;
-    private boolean[] primes;
+    private boolean[] primesCandidates;
+    private List<Integer> listOfPrimes;
 
     public SieveOfErastothenes(int n)
     {
         this.size = n;
-        this.primes = new boolean[n + 1];
+        this.primesCandidates = new boolean[n + 1]; //default is false
+        listOfPrimes = new ArrayList<>();
     }
 
+    private void generatePrimes(int n)
+    {
+        for(int i = 2; i <=n; i++)
+        {
+            for(int j = 2; j*i <=n; j++)
+            {
+                primesCandidates[i * j] = true;
+            }
+        }
 
+        for(int i = 2; i <=n; i++)
+        {
+            if(!primesCandidates[i])
+            {
+
+            }
+        }
+    }
 }
